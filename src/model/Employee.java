0b091f62ddf4947c90db05b1f406e7a9b23f7491
LeQuangGiaBao <baolqg1309@gmail.com>
2025-07-04 19:45:40 +0700

    Commit01:  Khởi tạo các class entity Employee, Staff, ViceManager, Manager, Department

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,4 +1,35 @@
 package model;
 
-public  class Employee {
+public abstract class Employee {
+    private String id;
+    private String name;
+    private int age;
+    private String address;
+    private double baseSalary;
+    private String position;
+    private int departmentId;
+
+    public Employee(String id, String name, int age, String address, double baseSalary, String position, int departmentId) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.baseSalary = baseSalary;
+        this.position = position;
+        this.departmentId = departmentId;
+    }
+
+    public String getId() { return id; }
+    public int getDepartmentId() { return departmentId; }
+    public String getPosition() { return position; }
+    public void setAddress(String address) { this.address = address; }
+    public void setBaseSalary(double baseSalary) { this.baseSalary = baseSalary; }
+    public double getBaseSalary() { return baseSalary; }
+
+    public abstract double calculateSalary();
+
+    public void showInfo() {
+        System.out.printf("ID: %s | Name: %s | Age: %d | Address: %s | BaseSalary: %.2f | Position: %s | DeptID: %d\n",
+                id, name, age, address, baseSalary, position, departmentId);
+    }
 }
--- src/model/ViceManager.java
+++ src/model/ViceManager.java
@@ -1,4 +1,12 @@
 package model;
 
-public class ViceManager {
+public class ViceManager extends Employee {
+    private static final double ALLOWANCE = 3000000;
+    public ViceManager(String id, String name, int age, String address, double baseSalary, int departmentId) {
+        super(id, name, age, address, baseSalary, "ViceManager", departmentId);
+    }
+    @Override
+    public double calculateSalary() {
+        return getBaseSalary() + ALLOWANCE;
+    }
 }
